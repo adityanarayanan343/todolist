311118ffe56342c5fffc80048097b60d87db7eb5
adityanarayanan343 <adityanarayanan343@gmail.com>
2024-06-01 12:04:24 +0530

    Some Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css'
+import { RiCheckboxBlankFill } from 'react-icons/ri'
+import { RiCheckboxFill } from 'react-icons/ri'
+import { RiDeleteBin6Fill } from 'react-icons/ri'
+import { TbPencilPlus } from 'react-icons/tb' 
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <>
+    <div className='header-container'>
+      <div className='header'>
+        <p className='header-title'>
+          My Tasks
+        </p>
+        <div className='header-add-task'>
+          <p className='header-add-task-text'>
+            <TbPencilPlus />
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className='to-do-list'>
+      <div className='to-do-container'>
+        <p className='to-do-checkbox'>
+          <RiCheckboxBlankFill />
+        </p>
+        <p className='to-do-text'>
+          React Tutorial
+        </p>
+        <p className='to-do-delete'>
+          <RiDeleteBin6Fill />
+        </p>
+      </div>
+    </div>
+  </>
   )
 }
 
